92195c8aa1a54052a044945e2945476c93c90b2d
Surya <thedevmystic@gmail.com>
2025-12-14 20:28:06 +0530

    Addition: Added PostCSS configuration file

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,49 @@
+/**
+ * Copyright 2025 Suryansh Singh
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * ------------------------------------------------------------------------------------------------------
+ *
+ * @path [ROOT]/postcss.config.mjs
+ * @file postcss.config.mjs
+ * @description Configuration file for PostCSS.
+ *
+ * @author thedevmystic (Surya)
+ * @copyright 2025 Suryansh Singh Apache-2.0 License
+ *
+ * SPDX-FileCopyrightText: 2025 Suryansh Singh
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+
+/**
+ * @description PostCSS Configuration.
+ */
 const config = {
   plugins: {
-    "@tailwindcss/postcss": {},
+    '@tailwindcss/postcss': {},
+    'postcss-flexbugs-fixes': {},
+    'postcss-preset-env': {
+      autoprefixer: {
+        flexbox: 'no-2009',
+       },
+       stage: 3,
+       features: {
+        'custom-properties': false,
+      },
+    },
   },
-};
+}
 
 export default config;
+
